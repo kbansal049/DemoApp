281fdf55c66639244a9bfa942112905870ebe359
kbansal049 <khushboo.bansal@grazitti.com>
2020-06-11 11:55:08 +0530

    intial commit

---
+++ force-app/main/default/aura/HelloWorld/HelloWorldController.js
@@ -0,0 +1,243 @@
+({
+    getListofSObjects: function (component, event, helper) {
+        
+        helper.test(component, event,helper);
+       
+    },
+    openPop : function(component, event, helper) {
+        var cmpTarget = component.find('pop');
+        $A.util.addClass(cmpTarget, 'slds-show');
+        $A.util.removeClass(cmpTarget, 'slds-hide');
+        helper.toggleHelper(component, event);
+        
+    },
+    
+    closePop : function(component, event, helper) {
+        var cmpTarget = component.find('pop');
+        // $A.util.addClass(cmpTarget, 'slds-hide');
+        //  $A.util.removeClass(cmpTarget, 'slds-show');
+        helper.toggleHelper1(component, event);
+        // $A.util.addClass(cmpTarget, 'slds-hide');
+        
+    },
+   
+    handleOnSavePrefrence: function (component, event, helper) {
+        var boolean;
+        var value = event.getSource().get("v.checked");
+        console.log("value", value);
+        if (value == true) {
+            var action = component.get("c.getCustomObjectData");
+            action.setCallback(this, function (response) {
+                console.log("response",response.getReturnValue());
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    if (response.getReturnValue().length > 0) {
+                        console.log("helllo", response.getReturnValue());
+                        var rvalue = response.getReturnValue();
+                        component.set("v.saveprefrence", value);
+                        console.log('return value',rvalue[0].Record_Type_Id__c);
+                        console.log('return value',rvalue[0].Record_Type_Label__c);
+                        if (rvalue[0].Record_Type_Id__c != undefined) {
+                            console.log("if case");
+                            component.set("v.message", true);
+                            component.set("v.message1", rvalue[0].Object_Name__c);
+                            component.set("v.message2", rvalue[0].Record_Type_Label__c);
+                            component.set("v.isOpen", true);
+                        } else {
+                            console.log("elsecalse");
+                            component.set("v.message", false);
+                            component.set("v.message1", rvalue[0].Object_Name__c);
+                            component.set("v.isOpen", true);
+                        }
+                        console.log("fwefgSGRGHE");
+                    } else {
+                        component.set("v.saveprefrence", value);
+                        helper.handleOnSavePrefrence1(component, event, helper);
+                    }
+                }
+            });
+            $A.enqueueAction(action);
+        } else if (value == false) {
+            component.set("v.saveprefrence", value);
+            helper.handleOnSavePrefrence1(component, event, helper);
+        }
+    },
+    handleOnSaveandNew: function (cmp, event, helper) {
+        console.log("value of clickedButton", cmp.get("v.clickedButton"));
+        cmp.set("v.clickedButton", "SaveandNew"); 
+        console.log("piyush0");
+        helper.handleOnSave1(cmp, event, helper);
+        console.log("iPHASEd");
+        component.set("v.reloadForm", false);
+        component.set("v.showForm", false);
+        
+    },
+    
+    handleSelectedObject: function (component, event, helper) {
+        component.set("v.showForm", false);
+        component.set("v.isAllSelected", false);
+
+        var sobjectname = event.getSource().get("v.value");
+        console.log("value", sobjectname);
+        component.set("v.RecordTypeId", '');
+        component.set("v.sObjectName", sobjectname);
+        console.log("erewergrregr", component.get("v.sObjectName"));
+        var action = component.get("c.getRecordTypeList");
+        action.setParams({
+            "sobjectname": sobjectname
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                if (response.getReturnValue().length > 0) {
+                    component.set("v.showRecordTypeOptions", true);
+                    var abc = response.getReturnValue()
+                    component.set("v.listOfRecordType", response.getReturnValue());
+                    var value = response.getReturnValue();
+                    component.set("v.RecordTypeId", value[0].Id);
+                    console.log('recordtypeid', component.get("v.RecordTypeId"));
+                    component.set("v.showForm", true);
+                    component.set("v.showButtons", true); 
+                    
+                } else {
+                    component.set("v.RecordTypeId", '');
+                    component.set("v.showRecordTypeOptions", false);
+                    component.set("v.showForm", true);
+                    component.set("v.showButtons", true); 
+                }
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    
+    handleSelectedRecordType: function (component, event, helper) {
+        component.set("v.showForm", false);
+		component.set("v.isAllSelected", false);
+
+        var objectname = component.get("v.sObjectName");
+        console.log("hello", objectname);
+        var recordTypeLabel = event.getSource().get("v.value");
+        console.log("recordTypeLabel", recordTypeLabel);
+        var action = component.get("c.getRecordTypeId");
+        action.setParams({
+            "recordTypeLabel": recordTypeLabel,
+            "objectname": objectname
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("helllo", response.getReturnValue());
+                component.set("v.RecordTypeId", response.getReturnValue());
+                console.log(component.get("v.RecordTypeId"));
+                component.set("v.showForm", true);
+                component.set("v.showButtons", true); 
+            }
+        });
+        $A.enqueueAction(action);
+    },
+    handleOnCreateNew: function (component, event, helper) {
+        component.set("v.reloadForm", false);
+        component.set("v.showButtons1", false); 
+        component.set("v.reloadForm", true);
+        component.set("v.showForm", true);
+        component.set("v.showButtons", true); 
+        
+        
+    },
+    
+    handleOnSuccess: function (component, event, helper) {
+        console.log("value of clickedButton", component.get("v.clickedButton"));
+        var id= event.getParam("id");
+        console.log("idddddddddddddddddpiyushddddd",id);
+        var objectname= component.get("v.sObjectName");
+        if (component.get("v.clickedButton") === "SaveandNew") {
+            console.log("if");
+            component.set("v.reloadForm", false);
+            component.set("v.showForm", false);
+            component.set("v.showButtons1", false); 
+            
+            component.set("v.reloadForm", true);
+            component.set("v.showForm", true);
+            component.set("v.showButtons", true); 
+            component.set("v.clickedButton", "false1");
+            
+            //component.set('v.counter',component.get('v.counter')+1); 
+            var action = component.get("c.getRecordName");
+            action.setParams({
+                "recordId": id,
+                "objectname": objectname
+            });
+            action.setCallback(this, function (response) {
+                var state = response.getState();
+                if (state === "SUCCESS") {
+                    console.log("oandey",response.getReturnValue());
+                    var recordDetail=component.get("v.RecordNameList");
+                    recordDetail.push(response.getReturnValue());
+                    component.set("v.RecordNameList",recordDetail);
+                    console.log("poandey",component.get("v.RecordNameList"));
+                    
+                }
+            });
+            $A.enqueueAction(action);
+            component.set("v.showRecordList",true);
+            
+        } else{
+            console.log("elese");
+            component.set("v.showButtons", false); 
+            
+            component.set("v.showButtons1", true);
+            component.set("v.showRecordList",false);
+            //component.set('v.counter',component.get('v.counter')+1); 
+            console.log("value"+component.get('v.counter'));
+        }
+        var toastEvent = $A.get("e.force:showToast");
+        toastEvent.setParams({
+            type: 'success',
+            mode: 'dismissible',
+            message: 'This is a required message',
+            messageTemplate: '{0} created! {1}!',
+            messageTemplateData: [objectname, {
+                url: '/lightning/r/'+objectname +'/'+id+'/'+'view',
+                label: 'Click here',
+            }]
+        });
+        toastEvent.fire();
+    },
+    
+    handleOnSave: function (component, event, helper) { 
+        console.log("value of clickedButton", component.get("v.clickedButton"));
+        console.log("save Button");
+        helper.handleOnSave1(component, event, helper);
+    },
+    handleOnCancel: function (component, event, helper) { 
+        console.log("handleOnCancel");
+        component.set("v.reloadForm", false);
+        component.set("v.reloadForm", true);
+    },
+    handleonError: function(component, event,handler) {
+        var error = event.getParam('output')['fieldErrors']['Name'][0]['message'];
+        console.log("response1", error);
+        var resultsToast = $A.get("e.force:showToast");
+        resultsToast.setParams({
+            "type":"error",
+            "title":"Error!",
+            "message": error
+        });
+        resultsToast.fire();
+    },
+    closeModel: function (component, event, helper) {
+component.set("v.isOpen", false);
+component.set("v.isAllSelected", false);
+},
+
+savePrefrence: function (component, event, helper) {
+// Display alert message on the click on the "Like and Close" button from Model Footer 
+// and set set the "isOpen" attribute to "False for close the model Box.
+
+component.set("v.isOpen", false);
+var value = component.get("v.saveprefrence");
+console.log("value", value);
+component.set("v.saveprefrence", value);
+helper.handleOnSavePrefrence1(component, event, helper);
+},
+})
---
+++ force-app/main/default/aura/HelloWorld/HelloWorldHelper.js
@@ -0,0 +1,117 @@
+({
+    test: function (component,event,helper) {
+        var action = component.get("c.initMethod");
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("TEST",response.getReturnValue());
+                component.set("v.listofsobject",response.getReturnValue().listofsobject);
+                
+                if((response.getReturnValue()).returnedCustomObjectData.length > 0)
+                {
+                    console.log("recordpssss");
+                    component.set("v.isAllSelected",((response.getReturnValue()).returnedCustomObjectData[0].Active__c));
+                    console.log("asfe",component.get("v.isAllSelected"));
+                    
+                    console.log("hello",component.get("v.selectedobject1"));
+                    if(((response.getReturnValue()).returnedCustomObjectData[0].Record_Type_Label__c) !=undefined &&  ((response.getReturnValue()).returnedCustomObjectData[0].Record_Type_Id__c) !=null)
+                    {  
+                        component.set("v.RecordTypeId",((response.getReturnValue()).returnedCustomObjectData[0].Record_Type_Id__c));
+                        component.set("v.selectedRecordType",((response.getReturnValue()).returnedCustomObjectData[0].Record_Type_Label__c));
+                        console.log("23456",component.get("v.returnedid"));
+                    } 
+                    component.set("v.selectedobject1",(response.getReturnValue()).returnedCustomObjectData[0].Object_Name__c);
+                    console.log("hello1",component.get("v.selectedobject1"));
+                      component.set("v.sObjectName",component.get("v.selectedobject1"));
+                }
+                else{
+                    console.log("hello");
+                    var abc = component.get("v.listofsobject");
+                    console.log("value1",abc);
+                    var count = abc.length;
+                    console.log("value2",abc.length);
+                    for(var i = 0; i <=count; i++){
+                        if(abc[i] == "Case"){
+                            console.log("insideloop");
+                            component.set("v.selectedobject1", abc[i]);
+                        }
+                    }
+                      component.set("v.sObjectName",component.get("v.selectedobject1"));
+                    component.set("v.isAllSelected", false);
+                    if((response.getReturnValue()).listOfRecordType.length > 0)
+                    {
+                        component.set("v.selectedRecordType",(response.getReturnValue()).listOfRecordType[0].Name);
+                        component.set("v.RecordTypeId",(response.getReturnValue()).listOfRecordType[0].Id);  
+                        console.log("p1",component.get("v.RecordTypeId"));
+                        
+                    }
+                }  
+              
+                console.log("p1",component.get("v.selectedRecordType"));
+                
+                if((response.getReturnValue()).listOfRecordType.length > 0)
+                {
+                    console.log("insideloop");
+                    component.set("v.showRecordTypeOptions", true);
+                    component.set("v.listOfRecordType",(response.getReturnValue()).listOfRecordType);
+                    console.log("insideloop",component.get("v.listOfRecordType"));
+                   
+                    
+                    
+                    
+                }
+                
+                
+                 
+                component.set("v.showForm", true);
+               
+                component.set("v.showButtons", true); 
+                 component.set("v.reloadForm", true);
+                
+                console.log("piyush");
+            }
+        });
+        
+        $A.enqueueAction(action);
+    },
+    handleOnSavePrefrence1:  function (component, event, helper) {
+        var sobjectname = component.get("v.sObjectName");
+        console.log("sobjectname", sobjectname);
+        var recordtypeid = component.get("v.RecordTypeId");
+        console.log("recordtypeid", recordtypeid);
+        var value = component.get("v.saveprefrence");
+        console.log("value", value);
+        var action = component.get("c.saveCustomObjectData");
+        action.setParams({
+            "ObjectName": sobjectname,
+            "RecordTypeId": recordtypeid,
+            "value": value
+        });
+        action.setCallback(this, function (response) {
+            var state = response.getState();
+            if (state === "SUCCESS") {
+                console.log("heeelooooopiyusgf", response.getReturnValue());
+            }
+        });   $A.enqueueAction(action); 
+    },
+    toggleHelper : function(component,event) {
+        var toggleText = component.find("pop");
+        $A.util.toggleClass(toggleText, "toggle");
+    },
+    toggleHelper1 : function(component,event) {
+        var toggleText = component.find("pop");
+        $A.util.toggleClass(toggleText, "toggle");
+    },
+    handleOnSave1: function (component, event, helper) {
+        event.preventDefault();
+        var globalId = component.get("v.dummy");
+        console.log("GID",globalId);
+        console.log("PIYUSH", component.find("recordForm"));
+        console.log("PIYUSH", component.find("recordForm")[0]);
+        if(component.find("recordForm")[0] != undefined)
+            component.find("recordForm")[0].submit();
+        else
+            component.find('recordForm').submit(); 
+    }
+    
+})
